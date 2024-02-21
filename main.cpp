418f1d2dc04ecc3cd4ea07f102a1fb7275db1238
Roberto VN <robertovanneto@gmail.com>
2024-02-21 15:44:49 -0300

    comentario e readme

--- main.cpp
+++ main.cpp
@@ -1,5 +1,23 @@
 #include "menu/menu.h"
 int main() {
+
+/*
+    Como executar:
+        1) No terminal, digite 'make' e espere a conclusão da compilação;
+        2) No terminal, digite './final'.
+*/
+
     Menu menu;
     return 0;
 }
+
+/*
+    Comecei a fazer o pre-desafio na madrugada que ele saiu e acabei completando os primeiros 4 desafios antes
+    de ver que poderia ser feito em qualquer linguagem, por isso decidi continuar com C++. Tentei fazer sem o
+    uso de outras bibliotecas, o que se mostrou ser uma tarefa bem mais dificil do que eu esperava (principalmente
+    a partir do desafio 6). Por nao usar bibliotecas, tive que implementar muitas coisas do zero, o que foi 
+    desafiador mas tambem recompensador. Estou feliz de conseguir terminar tudo no prazo, mas se eu tivesse
+    mais tempo tentaria fazer com o uso de alguma biblioteca que auxiliasse na resolucao desses desafios.
+
+    Obrigado e boa avaliação. :D                                                        Roberto Vandresen Neto
+*/
